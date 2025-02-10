a5abc49fac4734236fb38459a5e1ccbb910ea17b
Daniel Sloof <daniel@rubic.nl>
2025-02-10 10:34:19 +0100

    set up unit tests

---
+++ Test/Unit/Model/RuleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sansec\Shield\Test\Unit\Model;
+
+class RuleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
