405e50bb39a4510e021d3814f974d8cf880ec63e
Daniel Sloof <daniel@rubic.nl>
2025-07-22 16:24:23 +0200

    cleanup unused import

--- Model/Waf.php
+++ Model/Waf.php
@@ -3,7 +3,6 @@
 namespace Sansec\Shield\Model;
 
 use Magento\Framework\App\RequestInterface;
-use Sansec\Shield\Logger\Logger;
 use Sansec\Shield\Model\RuleFactory;
 use Sansec\Shield\Model\ConditionFactory;
 
@@ -12,11 +11,8 @@ class Waf
     /** @var Rule[] */
     private $rules = [];
 
-    public function __construct(
-        Rules $rules,
-        RuleFactory $ruleFactory,
-        ConditionFactory $conditionFactory
-    ) {
+    public function __construct(Rules $rules, RuleFactory $ruleFactory, ConditionFactory $conditionFactory)
+    {
         $ruleConfig = $rules->loadRules();
         foreach ($ruleConfig['rules'] ?? [] as $rule) {
             $conditions = [];
